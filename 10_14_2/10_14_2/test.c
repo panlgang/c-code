f9a07b908f42c4b9837ba74126575a26e759111b
c-code <2418512129@qq.com>
2018-10-14 21:47:23 +0800

    提交代码

---
+++ 10_14_2/10_14_2/test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 2
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{//×Ö·û²Ù×÷º¯Êý
+	printf("%c", tolower('A'));
+	printf("%c", toupper('a'));
+
+	system("pause");
+	return 0;
+}
